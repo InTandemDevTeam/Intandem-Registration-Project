73ccf3dad438d0ce525f1c28e245c041da2fb1a9
faizjamil <faizan34@gmail.com>
2019-08-19 10:56:16 -0400

    fixed db realted error

---
+++ InTandemRegistrationPortal/Migrations/20190819145225_FixDbIssue.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InTandemRegistrationPortal.Migrations
+{
+    public partial class FixDbIssue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
+                    Email = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Role = table.Column<string>(nullable: true),
+                    DateRegistered = table.Column<string>(nullable: true),
+                    Height = table.Column<string>(nullable: true),
+                    Weight = table.Column<string>(nullable: true),
+                    HasSeat = table.Column<bool>(nullable: true),
+                    HasBeenTrained = table.Column<bool>(nullable: true),
+                    TotalMilesTraveled = table.Column<int>(nullable: false),
+                    FromNYCares = table.Column<bool>(nullable: true),
+                    HasTandem = table.Column<bool>(nullable: true),
+                    HasSingleBike = table.Column<bool>(nullable: true),
+                    RiderLevel = table.Column<int>(nullable: true),
+                    Dog = table.Column<bool>(nullable: true),
+                    SpecialEquipment = table.Column<string>(nullable: true),
+                    HasBeenApproved = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RideEvent",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EventName = table.Column<string>(type: "varchar(300)", nullable: false),
+                    EventDate = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(type: "varchar(500)", nullable: false),
+                    Location = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Distance = table.Column<decimal>(type: "decimal(18, 1)", nullable: true),
+                    EventType = table.Column<int>(nullable: false),
+                    EventRatio = table.Column<string>(type: "varchar(50)", nullable: false),
+                    MaxSignup = table.Column<int>(nullable: true),
+                    MaxSignUpType = table.Column<int>(nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    ReasonForCancellation = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RideEvent", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RoleId = table.Column<string>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(nullable: false),
+                    ProviderKey = table.Column<string>(nullable: false),
+                    ProviderDisplayName = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    RoleId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    LoginProvider = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RideLeaderAssignment",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    InTandemUserID = table.Column<string>(nullable: true),
+                    RideEventID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RideLeaderAssignment", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_RideLeaderAssignment_AspNetUsers_InTandemUserID",
+                        column: x => x.InTandemUserID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RideLeaderAssignment_RideEvent_RideEventID",
+                        column: x => x.RideEventID,
+                        principalTable: "RideEvent",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RideRegistration",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RideEventID = table.Column<int>(nullable: false),
+                    InTandemUserID = table.Column<string>(nullable: true),
+                    RiderShowUp = table.Column<bool>(nullable: false),
+                    Miles = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RideRegistration", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_RideRegistration_AspNetUsers_InTandemUserID",
+                        column: x => x.InTandemUserID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RideRegistration_RideEvent_RideEventID",
+                        column: x => x.RideEventID,
+                        principalTable: "RideEvent",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RideLeaderAssignment_InTandemUserID",
+                table: "RideLeaderAssignment",
+                column: "InTandemUserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RideLeaderAssignment_RideEventID",
+                table: "RideLeaderAssignment",
+                column: "RideEventID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RideRegistration_InTandemUserID",
+                table: "RideRegistration",
+                column: "InTandemUserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RideRegistration_RideEventID",
+                table: "RideRegistration",
+                column: "RideEventID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "RideLeaderAssignment");
+
+            migrationBuilder.DropTable(
+                name: "RideRegistration");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "RideEvent");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Contains all code related to the new registration section for the InTandem websi
 * .NET Core SDK 2.2
 * Visual Studio 2019 (version 16)
 
-### To avoid any database-realted errors
+### To avoid any database-related errors
 
 Run the following command in the NuGet Package Manager (PMC) in Visual Studio 2019 (found in Tools > NuGet Package Manager > Package Manger Console)
 
@@ -17,17 +17,18 @@ Run the following command in the NuGet Package Manager (PMC) in Visual Studio 20
 
 This command will create the database that this web application uses
 
-If you encounter any database-realted errors, you may drop the database by using the folliwing command in the PMC to see if it fixes the error
+If you encounter any database-related errors, you may drop the database by using the folliwing command in the PMC to see if it fixes the error
 
     PM> Drop-Database
 
-In order to be able to deploy this web application the **Expressive Attributes** package must be installed
-Run the following command to install it in the Package Manager Console\
+In order to be able to deploy this web application the **Expressive Attributes** package must be installed.
+
+Run the following command to install it in the Package Manager Console
     
     PM> Install-Package ExpressiveAnnotations
 
 
-###Setup account confirmation
+### Setup account confirmation
 
 In order to be able to send account confirmation emails via SendGrid, run the following commands if you do not want to use your own SendGrid account
 
@@ -39,7 +40,7 @@ If you would like to use your own SendGrid account, replace the API key and your
 These two commands create secrets used by Visual Studio, they are stored in secrets.json which is located in `%APPDATA%/Microsoft/UserSecrets/<WebAppName-userSecretsId>`
 
 
-###Setup Seed Data Account
+### Setup Seed Data Account
 
 In order to be able to sign in as the seeded users, you will need to set up a password using the Secrets Tool:
 
